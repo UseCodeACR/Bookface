e72fcc70221c7644b54e5389829d90ee3de65d2d
UseCodeACR <104840571+UseCodeACR@users.noreply.github.com>
2023-01-05 13:42:17 +0000

    updates

--- login.php
+++ login.php
@@ -14,43 +14,6 @@ session_start()
 <body>
 <?php include "templates/nav.php"; ?>
 
-    <?php /*
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">BookFace</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarColor01">
-        <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-            <a class="nav-link" href="index.php">Home
-                <span class="visually-hidden">(current)</span>
-            </a>
-            </li>
-            <?php if (!isset($_SESSION['loggedin'])):?>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php">Login</a>
-                </li>
-            <?php endif;?>
-            <?php if (isset($_SESSION['loggedin'])):?>
-                <li class="nav-item">
-                <a class="nav-link" href="messages.php">Messages</a>
-                </li>
-            <?php endif;?>
-            <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-            </li>
-        </ul>
-        <?php if(connect()):?>
-            <span class="badge rounded-pill bg-success">Connected</span>
-        <?php else:?>
-            <span class="badge rounded-pill bg-danger">Failed</span>
-        <?php endif;?>
-        </div>
-    </div>
-    </nav>
-    */?>
 
     <?php
 
@@ -61,6 +24,7 @@ session_start()
         </div>";
         unset($_SESSION["loginerror"]);
     }
+
     ?>
 
 
